34334c27c5a1f9f4e92e97d94a8d21fdee26c4e6
JesseOSullivan <105776319+JesseOSullivan@users.noreply.github.com>
2023-07-15 17:43:18 +1000

    refactor

--- app.js
+++ app.js
@@ -1,26 +1,27 @@
 const express = require('express');
-const {gptFetch} = require('./gpt')
-// Create an instance of Express
+const { gptFetch } = require('./gpt');
+
 const app = express();
 
-// Define a route for the root URL
+// Enable JSON body parsing
+app.use(express.json());
+
 app.post('/', async (req, res) => {
-    const messages = [
-        { role: 'system', content: 'you are afood recipe expert. user will request how to make a dish or ask you to sugget one and you will proived a list' },
-        { role: 'user', content:req },
-      ];
-        
-    try {
-      const gptResponse = await gptFetch({prompt: messages});
-      res.send("gptResponse");
-    } catch (err) {
-      console.error(err);
-      res.status(500).send('Error occurred while fetching from GPT-3');
-    }
-  });
-  
-// Start the server
-const port = 3000; // You can change this to any port number you prefer
+  const messages = [
+    { role: 'system', content: 'you are a food recipe expert. user will request how to make a dish or ask you to suggest one and you will provide a list' },
+    { role: 'user', content: req.body.content },
+  ];
+      
+  try {
+    const gptResponse = await gptFetch({ prompt: messages });
+    res.send(gptResponse);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error occurred while fetching from GPT-3');
+  }
+});
+
+const port = 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
--- gpt.js
+++ gpt.js
@@ -1,35 +1,29 @@
 const axios = require('axios');
-
 exports.gptFetch = async ({ prompt }) => {
-    const apiKey = process.env.AI_KEY // Replace with your API key
-
+    const apiKey = process.env.AI_KEY
     const endpoint = 'https://api.openai.com/v1/chat/completions'
-    
+        
     try {
+      const response = await axios.post(
+        endpoint,
+        {
+          model: 'gpt-3.5-turbo',
+          messages: prompt
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + apiKey,
+          },
+        }
+      );
     
-            const response = await axios.post(
-              endpoint,
-              {
-                model: 'gpt-3.5-turbo',
-                messages: prompt
-        
-              },
-              {
-                headers: {
-                  'Content-Type': 'application/json',
-                  'Authorization': 'Bearer ' + apiKey,
-                },
-              }
-            );
-        
-            console.log('GPT-3 response:', response.data.choices[0].message.content);
-            return response.data.choices[0].message.content
-        
-          } catch (error) {
-            console.error(error);
-          }
-        };
-
-
-
-
+      console.log('GPT-3 response:', response.data.choices[0].message.content);
+      return response.data.choices[0].message.content
+    
+    } catch (error) {
+      console.error(error);
+      throw error; // Throw the error to be caught by Express's error handler
+    }
+  };
+  
