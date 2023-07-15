b77cbf1a687918e51753b852e10a06d0a351b7d5
JesseOSullivan <105776319+JesseOSullivan@users.noreply.github.com>
2023-07-15 17:38:57 +1000

    Update app.js

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ const express = require('express');
 const {gptFetch} = require('./gpt')
 // Create an instance of Express
 const app = express();
-const messages = [
-    { role: 'system', content: 'you are afood recipe expert.' },
-    { role: 'user', content: 'hwo to make tacos' },
-  ];
 
 // Define a route for the root URL
-app.get('/', async (req, res) => {
+app.post('/', async (req, res) => {
+    const messages = [
+        { role: 'system', content: 'you are afood recipe expert. user will request how to make a dish or ask you to sugget one and you will proived a list' },
+        { role: 'user', content:req },
+      ];
+        
     try {
       const gptResponse = await gptFetch({prompt: messages});
       res.send(gptResponse);
