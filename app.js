e4ef496315b99c4aaea09c3360e0af88fe053692
JesseOSullivan <105776319+JesseOSullivan@users.noreply.github.com>
2023-07-15 19:37:41 +1000

    Update app.js

--- app.js
+++ app.js
@@ -8,8 +8,7 @@ app.use(express.json());
 
 app.post('/', async (req, res) => {
     const messages = [
-      { role: 'system', content: 'you are a food recipe expert. user will request how to make a dish or ask you to suggest one and list ingredients needed under recipe and the instructions under instructions' },
-      { role: 'user', content: req.body.content },
+      { role: 'system', content: 'you are a food recipe expert. user will request how to make a dish or ask you to suggest one then list the ingredients and the dish"'}
     ];
   
     try {
@@ -18,7 +17,7 @@ app.post('/', async (req, res) => {
       // Add the assistant's message to the conversation history
       messages.push({ role: 'assistant', content: gptResponse });
   
-      const productNames = ["cheese 1kg", "ham 100grams", "bread", "cheese slices 10pc"];
+      const productNames = ["cheese 1kg 10$", "ham 100grams", "bread 2$", "cheese slices 10pc, 3$", 'coles butter block'];
   
       /*const products = await faunaFetch({
         query: `
@@ -39,7 +38,7 @@ app.post('/', async (req, res) => {
      // const productsString = products.data.getProductsByNames.data.map(product => `${product.name}: ${product.price}`).join(', ');
   
       // Add the next user's message to the conversation history
-      messages.push({ role: 'user', content: `Using the above recipe as well as the following list of products from Woolworths (${productNames}), I would like you to return a structured list of products we can use to efficiently make this recipe.` });
+      messages.push({ role: 'user', content: `Using the above recipe as well as the following list of products from Woolworths (${productNames}), I would like you to return a structured list of products from the list provided we can use to efficiently make this recipe.` });
   
       gptResponse = await gptFetch({ prompt: messages });
   
