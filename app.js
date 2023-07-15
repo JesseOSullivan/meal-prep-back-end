118f195029e5c36f84534080a2ebdad0ca1c34da
JesseOSullivan <105776319+JesseOSullivan@users.noreply.github.com>
2023-07-15 18:08:49 +1000

    Update app.js

--- app.js
+++ app.js
@@ -7,21 +7,26 @@ const app = express();
 app.use(express.json());
 
 app.post('/', async (req, res) => {
-  const messages = [
-    { role: 'system', content: 'you are a food recipe expert. user will request how to make a dish or ask you to suggest one and the response will be a json object the recipe under recipe and the instructions under instructions' },
-    { role: 'user', content: req.body.content },
-  ];
-      
-  try {
-    const gptResponse = await gptFetch({ prompt: messages });
-    res.send(gptResponse.instructions);
-  } 
-  catch (err) {
-    console.error(err);
-    res.status(500).send('Error occurred while fetching from GPT-3');
-  }
-});
-
+    const messages = [
+      { role: 'system', content: 'you are a food recipe expert. user will request how to make a dish or ask you to suggest one and the response will be a json object the list of ingedients needed under recipe and the instructions under instructions' },
+      { role: 'user', content: req.body.content },
+    ];
+        
+    try {
+      const gptResponse = await gptFetch({ prompt: messages });
+      // Split the text by the first newline character
+      const [recipe, ...instructionsArr] = gptResponse.split('\n');
+      // Join the remaining parts back together for the instructions
+      const instructions = instructionsArr.join('\n');
+      // Send back a JSON object with the parsed recipe and instructions
+      res.json({ recipe, instructions });
+    } 
+    catch (err) {
+      console.error(err);
+      res.status(500).send('Error occurred while fetching from GPT-3');
+    }
+  });
+  
 //messages.push({role: 'user', content:req})
 
 const port = 3000;
