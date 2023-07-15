ddd0493625c0d61bf1078244ac208cdcf2221518
JesseOSullivan <105776319+JesseOSullivan@users.noreply.github.com>
2023-07-15 18:27:16 +1000

    Update app.js

--- app.js
+++ app.js
@@ -8,26 +8,30 @@ app.use(express.json());
 
 app.post('/', async (req, res) => {
     const messages = [
-      { role: 'system', content: 'you are a food recipe expert. user will request how to make a dish or ask you to suggest one and list  ingedients needed under recipe and the instructions under instructions' },
+      { role: 'system', content: 'you are a food recipe expert. user will request how to make a dish or ask you to suggest one and list ingredients needed under recipe and the instructions under instructions' },
       { role: 'user', content: req.body.content },
     ];
         
     try {
-      const gptResponse = await gptFetch({ prompt: messages });
-
-
-        messages.push({role: 'user', content:"using the below recipe as well as the below list of products from woolwroths I would like you to return a structured list of products we can use to efficiently make this recipe", gptResponse})
-
-        const productRecipe = await gptFetch({ prompt: messages });
-
-      res.send(productRecipe );
+      let gptResponse = await gptFetch({ prompt: messages });
+  
+      // Add the assistant's message to the conversation history
+      messages.push({ role: 'assistant', content: gptResponse });
+  
+      // Add the next user's message to the conversation history
+      messages.push({ role: 'user', content: "Using the below recipe as well as the below list of products from Woolworths, I would like you to return a structured list of products we can use to efficiently make this recipe" });
+          
+      // Make another call to gptFetch with the updated conversation history
+      gptResponse = await gptFetch({ prompt: messages });
+  
+      res.send(gptResponse);
     } 
     catch (err) {
       console.error(err);
       res.status(500).send('Error occurred while fetching from GPT-3');
     }
   });
-  
+    
 //messages.push({role: 'user', content:req})
 
 const port = 3000;
