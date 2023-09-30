eec64225fba22c3ece4e85c9c682a2d1638d8c28
JesseOSullivan <105776319+JesseOSullivan@users.noreply.github.com>
2023-09-30 15:00:19 +1000

    Update app.js

--- app.js
+++ app.js
@@ -1,57 +1,39 @@
 const express = require('express');
-const { gptFetch } = require('./external/gpt');
+const axios = require('axios');
+
 const app = express();
-const { fetchProductData } = require('./models/productModel');
 
+app.use(express.json()); 
 
-// Enable JSON body parsing
-app.use(express.json());
-app.use((req, res, next) => {
-
-res.header('Access-Control-Allow-Origin', '*');
-res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-next();
-});
-
-app.post('/recipe', async (req, res) => {
-    const messages = [
-        { role: 'system', content: 'you are a food expert. User will request a recipe or instructions. Please provide a complete response as json with these properties. ingredients in an array (name and ammount only) instructions and ingredientNoAmt for only the name of ingredient.' },
-        { role: 'user', content: req.body.content }
-    ];
-  
+// Endpoint to get a response from GPT-3.
+app.post('/get-gpt-response', async (req, res) => {
     try {
-      let gptResponse = await gptFetch({ prompt: messages });
-      console.log("json data ");
-      var data = JSON.parse(gptResponse);
-      var recipe = data.recipe;
-      var ingredients = data.ingredients;
-      
-      console.log(data)
+        const apiKey = process.env.AI_KEY;
+        const prompt = req.body.prompt;
+        
+        const endpoint = 'https://api.openai.com/v1/chat/completions';
+        const response = await axios.post(
+            endpoint,
+            {
+                model: 'gpt-3.5-turbo-16k-0613',
+                messages: [{ role: 'user', content: prompt }]
+            },
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + apiKey,
+                },
+            }
+        );
 
+        const gptResponse = response.data.choices[0].message.content;
+        res.json({ response: gptResponse });
 
-      // Add the assistant's message to the conversation history
-      messages.push({ role: 'assistant', content: gptResponse });
-      
-      const productNames = data.ingredients;
-
-      const productData = await fetchProductData(productNames);
-      // Add the next user's message to the conversation history
-      
-      let jsonString = JSON.stringify(productData);
-
-      messages.push({ role: 'user', content: `Using the above recipe as well as the following list of products (${jsonString}), I would like you to return a structured list of products from the list provided we can use to efficiently make this recipe. Only use what is necesary return it as anwith the property name products array whith each array including the product name, amt and link use the links are from the object above also add the instructions ` });
-
-      gptResponse = await gptFetch({ prompt: messages });
-  
-      res.send(gptResponse);
-    } 
-    catch (err) {
-      console.error(err);
-      res.status(500).send(`Error occurred while fetching from GPT-3: ${err.message}`);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Internal Server Error" });
     }
 });
 
-const port = 3000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+const PORT = 3001;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
